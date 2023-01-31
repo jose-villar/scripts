d99543a43c0afa8f0e90973df467d5433cf4e24e
Jose Villar <8.villar@gmail.com>
2023-01-31 00:10:37 -0300

    feat(utils): create script to find duplicate words

---
+++ find_duplicate_words.sh
@@ -0,0 +1,13 @@
+#!/bin/env sh
+
+#******************************************************************************#
+#                           find_duplicate_words.sh                            #
+#                      Highlight consecutive repeated words                    #
+#                  Ex usage: find_duplicate_words.sh input.txt                 #
+#******************************************************************************#
+
+FindDuplicateWords() {
+  rg --pcre2 -U "\b(\w+)\s+\1\b" "$1"
+}
+
+FindDuplicateWords "$1"
